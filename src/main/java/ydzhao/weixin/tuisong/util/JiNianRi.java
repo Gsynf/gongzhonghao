ce09ad9745864310f09871dd0b9ae8607afc76f0
Gsynf <18813058359@163.com>
2022-08-11 23:31:21 +0800

    20220811

---
+++ src/main/java/ydzhao/weixin/tuisong/util/JiNianRi.java
@@ -0,0 +1,93 @@
+package ydzhao.weixin.tuisong.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+/**
+ * @ClassName JiNianRi
+ * @Description TODO
+ * @Author ydzhao
+ * @Date 2022/8/2 17:32
+ */
+public class JiNianRi {
+    /**
+     * 恋爱
+     */
+    static String lianAi = "2016-08-25";
+    /**
+     * 领证
+     */
+    static String linZheng = "2022-03-19";
+    /**
+     * 结婚
+     */
+    static String jieHun = "2022-07-08";
+    /**
+     * 生日
+     */
+    static String shengRi = "2022-08-18";
+    /**
+     * 团聚
+     */
+    static String tuanJu = "2023-06-30";
+
+
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    /**
+     * 距离date还有多少天
+     * @param date
+     * @return
+     */
+    public static int before(String date) {
+        int day = 0;
+        try {
+            long time = simpleDateFormat.parse(date).getTime() - System.currentTimeMillis();
+            day = (int) (time / 86400000L);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return day;
+    }
+
+
+    /**
+     * 已经过去date多少天
+     * @param date
+     * @return
+     */
+    public static int after(String date) {
+        int day = 0;
+        try {
+            long time = System.currentTimeMillis() - simpleDateFormat.parse(date).getTime();
+            day = (int) (time / 86400000L);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return day;
+    }
+
+    public static int getJieHun() {
+        return before(jieHun);
+    }
+
+    public static int getLinZhen() {
+        return before(linZheng);
+    }
+    public static int getTuanJu() {
+        return before(tuanJu);
+    }
+    public static int getLianAi() {
+        return after(lianAi);
+    }
+
+    public static int getShengRi(){
+        return after(shengRi);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getJieHun());
+    }
+
+
+}
